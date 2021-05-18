d738b8c3881015badf1346b18883c4a26e834162
Javier Alejandro Frias Navas <j.frias.di@gmail.com>
2021-05-18 03:08:31 -0500

    Update

---
+++ pruebaEquitel/DataAnalyse.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace pruebaEquitel
+{
+    class DataAnalyse
+    {
+        public static bool AnalyseText(string inputFilePath, string outputFilePath)
+        {
+            List<int> results = new List<int>();
+
+            var file = loadFile(inputFilePath);
+
+            File.AppendAllText(outputFilePath, $"\n\n\nResultados de la ejecucion realizada en la fecha {DateTime.Now} \n\n");
+
+            
+
+            var wordsFinalNTask =   wordsFinalN(file, outputFilePath);
+            var fifteenWordsTask =  fifteenWords(file, outputFilePath);
+            var paragraphCounterTask =  paragraphCounter(file, outputFilePath);
+            var distinctNCharTask =  distinctNChar(file, outputFilePath);
+
+            return true;
+        }
+        static string loadFile (string filePath)
+        {
+            string file = File.ReadAllText(filePath);
+
+            return file;
+        }
+        static int wordsFinalN(string file, string filePath)
+        {
+            int contador = 0;
+            var pattern = @"\b\w+n\b";
+
+            contador = Regex.Matches(file, pattern).Count;
+            File.AppendAllTextAsync(filePath, $"La cantidad de palabras que terminan en N o n es de: {contador}\n");
+            
+            return contador;
+        }
+        static int fifteenWords(string file, string filePath)
+        {
+            int counter = 0;
+            var pattern = @"(([\-_\('’""]*\b\w+\b[ \),:;\-_'’""]*){16,})\.[ ]";
+
+            counter = Regex.Matches(file, pattern).Count;
+            File.AppendAllTextAsync(filePath, $"El numero de frases que contiene mas de 15 palabras es de: {counter}\n");
+
+
+            return counter;
+        }
+        static int paragraphCounter(string file, string filePath)
+        {
+            int counter = 0;
+            var pattern = @"\.\r";
+
+
+            counter = Regex.Matches(file, pattern).Count;
+            File.AppendAllTextAsync(filePath, $"El numero de parrafos en el texto es de: {counter}\n");
+
+
+            return counter;
+        }
+        static int distinctNChar(string file, string filePath)
+        {
+            int counter = 0;
+            var pattern = @"[^nN\s\p{P}]";
+
+
+                counter = Regex.Matches(file, pattern).Count;
+                File.AppendAllTextAsync(filePath, $"La cantidad de caracteres diferentes a N o n es: {counter}\n");
+
+
+            return counter;
+        }
+    }
+}
--- pruebaEquitel/Program.cs
+++ pruebaEquitel/Program.cs
@@ -2,6 +2,7 @@
 using System.IO;
 using System.Collections.Generic;
 using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 
 namespace pruebaEquitel
 {
@@ -13,64 +14,20 @@ namespace pruebaEquitel
             var outputFileName = "Result.txt";
             var currentDirectory = Directory.GetCurrentDirectory();
 
-            var inputFilePath = Path.Combine(currentDirectory,"txt", inputFileName);
-            var outputFilePath = Path.Combine(currentDirectory, "txt", outputFileName);
+            string inputFilePath = Path.Combine(currentDirectory,"txt", inputFileName);
+            string outputFilePath = Path.Combine(currentDirectory, "txt", outputFileName);
 
-            AnalyseText(inputFilePath, outputFilePath);
-        }
-        static void AnalyseText(string inputFilePath, string outputFilePath)
-        {
-            int counter;
+            var analyseDataTask = DataAnalyse.AnalyseText(inputFilePath, outputFilePath);
 
-            List<string> results = new List<string>();
+            //bool analyseDataResult = analyseDataTask
 
-            var inputFile = File.ReadAllText(inputFilePath);
+            //if (analyseDataResult)
+            //{
+            //    Console.WriteLine($"El archivo {inputFileName} fue analizado con exito");
+            //    Console.WriteLine($"Los resultados se pueden encontrar en el archivo {outputFileName}");
+            //}
 
-            File.AppendAllTextAsync(outputFilePath, $"\n\n\nResultados de la ejecucion realizada en la fecha {DateTime.UtcNow} \n\n");
 
-            counter = wordsFinalN(inputFile);
-            File.AppendAllTextAsync(outputFilePath, $"La cantidad de palabras que terminan en N o n es de: {counter}\n");
-            
-            counter = fifteenWords(inputFile);
-            File.AppendAllTextAsync(outputFilePath, $"El numero de frases que contiene mas de 15 palabras es de: {counter}\n");
-
-            counter = paragraphCounter(inputFile);
-            File.AppendAllTextAsync(outputFilePath, $"El numero de parrafos en el texto es de: {counter}\n");
-
-            counter = distinctNChar(inputFile);
-            File.AppendAllTextAsync(outputFilePath, $"La cantidad de caracteres diferentes a N o n es: {counter}\n");
-        }
-        static int wordsFinalN (string file)
-        {
-            int contador;
-            var pattern = @"\b\w+n\b";
-            contador = Regex.Matches(file, pattern).Count;
-
-            return contador;
-        }
-        static int fifteenWords(string file)
-        {
-            int contador;
-            var pattern = @"(([\-_\('’""]*\b\w+\b[ \),:;\-_'’""]*){16,})\.[ ]";
-            contador = Regex.Matches(file, pattern).Count;
-
-            return contador;
-        }
-        static int paragraphCounter (string file)
-        {
-            int contador;
-            var pattern = @"\.\r";
-            contador = Regex.Matches(file, pattern).Count;
-
-            return contador;
-        }
-        static int distinctNChar(string file)
-        {
-            int contador;
-            var pattern = @"[^nN\s\p{P}]";
-            contador = Regex.Matches(file, pattern).Count;
-
-            return contador;
         }
     }
 }
